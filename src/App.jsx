91ec1e0055c4a9126e9121a725b4f3a8cd75267c
tazim5032 <51076835+tazim5032@users.noreply.github.com>
2024-03-15 15:31:50 +0600

    delete button added

--- src/App.jsx
+++ src/App.jsx
@@ -27,6 +27,11 @@ function App() {
     }
   }
   console.log(cart);
+
+  const handleDelete = id =>{
+    const newCart = cart.filter(item => item.id!=id);
+    setCart(newCart);
+  }
   return (
     <>
       <div className="main-container lg:flex justify-between mx-[6%] mt-6">
@@ -44,16 +49,21 @@ function App() {
           <h1 className='text-3xl font-bold text-center my-6'>Your Cart</h1>
 
           <div className='flex justify-around'>
+            <h1 className='text-xl font-medium'>Serial</h1>
             <h1 className='text-xl font-medium'>Title</h1>
             <h1 className='text-xl font-medium'>Price</h1>
+            <h1 className='font-medium'>Remove</h1>
           </div>
 
           <div className="cart-info">
             {
-              cart.map((item,idx) =>(
-                <div key={idx} className='flex justify-around mt-4'>
+              cart.map((item,id) =>(
+                <div key={id} className='flex justify-around mt-4'>
+                  <h1 className='text-xl font-medium'>{id+1}</h1>
                   <h1 className='text-xl font-medium'>{item.title.slice(0,10)}</h1>
                   <h1 className='text-xl font-medium'>${item.price}</h1>
+                  <button onClick={() => handleDelete(item.id)}
+                   className='bg-red-500 p-2 rounded-xl font-normal text-white'>Remove</button>
                 </div>
               ))
             }
